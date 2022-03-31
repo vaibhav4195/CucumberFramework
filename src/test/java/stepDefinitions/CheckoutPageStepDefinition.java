00d6bb30c1d165878121dbd1aa11fc5180495c0b
Vaibhav Pawde <vaibhav4195@gmail.com>
2022-03-31 13:26:42 +0530

    To add newly created devlop branch

--- src/test/java/stepDefinitions/CheckoutPageStepDefinition.java
+++ src/test/java/stepDefinitions/CheckoutPageStepDefinition.java
@@ -21,12 +21,13 @@ public class CheckoutPageStepDefinition {
 	@Then("^User proceeds to checkout and validate the (.+) items in checkout page$")
 	public void user_proceeds_to_checkout_and_validate_the_items_in_checkout_page(String name) throws Exception {
 		checkoutPage.CheckOutItems();
+		System.out.println("User switch to check out page");
 		Thread.sleep(3000);
 	}
 	
 	@Then("Verify user has ability to enter promo code and place the order")
 	public void verify_user_has_ability_to_enter_promo_code_and_place_the_order() {
-		
+		System.out.println("Validating promo code and place order button");
 		Assert.assertTrue(checkoutPage.verifyPromoBtn());
 		Assert.assertTrue(checkoutPage.verifyPlaceOrder());
 	}
